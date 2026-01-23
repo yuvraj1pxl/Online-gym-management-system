0416d158e00cd999ace1d7451f30983efc5b349b
Yuvraj Prajapati <yuvrajprajapati5665@gmail.com>
2026-01-23 19:22:13 +0530

    Prepare project for deployment

--- gym/settings.py
+++ gym/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
 ]
 
 ROOT_URLCONF = 'gym.urls'
