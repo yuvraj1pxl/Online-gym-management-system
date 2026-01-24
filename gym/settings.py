4ad45e5c586d523430c80efbf5b6cbcd954845af
Yuvraj Prajapati <yuvrajprajapati5665@gmail.com>
2026-01-24 09:31:59 +0530

    Fix invalid middleware config

--- gym/settings.py
+++ gym/settings.py
@@ -53,17 +53,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
 ]
-
 ROOT_URLCONF = 'gym.urls'
 
 TEMPLATES = [
@@ -148,3 +146,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # media (for uploaded profile pics)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
